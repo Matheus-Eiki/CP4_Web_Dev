40ff201b3ed2eaf1883c50694deff23825d6fce4
Matheus <m.eiki.ikeda@gmail.com>
2025-03-20 19:47:29 -0300

    Criando a página error e Home e importando ambas para o App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Error from './routes/Error';
+import Home from './routes/Home';
+
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Error/>}/>
+          <Route path="/" element={<Home/>}/>  
+        </Routes> 
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/routes/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = ()=> {
+    return (
+        <section>
+            <h2>404 Error Page not Found</h2>
+        </section>
+    )
+}
+
+export default Error 
