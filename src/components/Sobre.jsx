84276a462cf9f574a18e54dd1e4b416c0bea53de
Francisco <franciscovargasmarcal@gmail.com>
2025-03-21 20:52:54 -0300

    Criando section sobre

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Sobre = () => {
   return (
-    <section>
+    <section className='sobre'>
         <h1>Saiba mais</h1>
+        <div className='c-sobre'>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi nisi impedit dolores, earum error perferendis soluta odit, recusandae consectetur rem laudantium repellendus ut maxime exercitationem rerum sed, id assumenda labore.</p>
+        </div>
     </section>
   )
 }
