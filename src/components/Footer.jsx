2b74da066219ce387824807390cc6ab9e778f642
Marcelo <marcelo.anonimo.2006@gmail.com>
2025-03-22 17:30:50 -0300

    finalizando o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
       <footer className='rodape'>
-          <h6>2025 | Todos os direitos reservados | Code Nexus</h6>
+          <h6>2025 | Todos os direitos reservados| Telefone: (99) 91234-5678 | email: codenexus@gmail.com | Code Nexus</h6>
       </footer>
     )
   }
