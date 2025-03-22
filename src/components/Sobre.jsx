8c883d94dbfd929f00bd199f12b546741d5b02f3
Francisco <franciscovargasmarcal@gmail.com>
2025-03-22 19:22:09 -0300

    finalizando os ajustes na page sobre

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -6,7 +6,7 @@ import gif from '../assets/gif.gif';
 const Sobre = () => {
   return (
     <section className='sobre'>
-        <h1>Saiba mais</h1>
+        <h1 id='title-sobre'>Saiba mais</h1>
         <img src={gif} alt="Gif-promocao-camisa-brasil" className="gif"/>
         <div className='c-sobre'>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi nisi impedit dolores, earum error perferendis soluta odit, recusandae consectetur rem laudantium repellendus ut maxime exercitationem rerum sed, id assumenda labore.</p>
