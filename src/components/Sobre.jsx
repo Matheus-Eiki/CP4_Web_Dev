7ecfd6527d43beb801b619e48dc5a098d1769c96
Francisco <franciscovargasmarcal@gmail.com>
2025-03-22 18:16:44 -0300

    personalizando e estilizando a pagina sobre

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+// importando imagens
+import gif from '../assets/gif.gif';
+
 const Sobre = () => {
   return (
     <section className='sobre'>
         <h1>Saiba mais</h1>
+        <img src={gif} alt="Gif-promocao-camisa-brasil" className="gif"/>
         <div className='c-sobre'>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi nisi impedit dolores, earum error perferendis soluta odit, recusandae consectetur rem laudantium repellendus ut maxime exercitationem rerum sed, id assumenda labore.</p>
         </div>
