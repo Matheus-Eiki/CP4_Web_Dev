7766d7824dcf989b03ffe526cc31f74f4eda8e42
Francisco <franciscovargasmarcal@gmail.com>
2025-03-22 19:21:47 -0300

    finalizando os ajustes na page produtos

--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -13,14 +13,34 @@ const Produto = () => {
     <section className='produto'>
       <h1>Camisas disponíveis</h1>
       <div className='c-produto'>
-        <img src={produto1} alt="Camiseta Corinthians" className="img"/>
+        <div className='prod'>
+          <h4>Corinthians | 2023</h4>
+          <img src={produto1} alt="Camiseta Corinthians" className="img"/>
+        </div>
+        <div className='prod'>
+          <h4>Santos | 2021</h4>
         <img src={produto2} alt="Camiseta Santos" className="img"/>
-        <img src={produto3} alt="Camiseta Seleção Brasil" className="img"/>
+        </div>
+        <div className='prod'>
+          <h4>Brasil | 2020</h4>
+          <img src={produto3} alt="Camiseta Seleção Brasil" className="img"/>
+        </div>
       </div>
+      <div className='container-produtos'>
       <div className='c-produto'>
-        <img src={produto4} alt="Camiseta Clube do Remo" className="img"/>
-        <img src={produto5} alt="Camiseta Real Madrid" className="img"/>
-        <img src={produto6} alt="Camiseta Liverpool" className="img"/>
+        <div className='prod'>
+          <h4>Clube do Remo | 2022</h4>
+          <img src={produto4} alt="Camiseta Clube do Remo" className="img"/>
+        </div>
+        <div className='prod'>
+          <h4>Real Madrid | 2018</h4>
+          <img src={produto5} alt="Camiseta Real Madrid" className="img"/>
+        </div>
+        <div className='prod'>
+          <h4>Liverpool | 2017</h4>
+          <img src={produto6} alt="Camiseta Liverpool" className="img"/>
+        </div>
+      </div>
       </div>
     </section>
   )
